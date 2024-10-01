8f1ec1e8a4abf496b85d7847424b2ecc0c957d3e
emegua <emeguag@gmail.com>
2024-10-01 11:30:56 -0700

    add sample st programs

--- webserver/core/POUS.c
+++ webserver/core/POUS.c
@@ -1,19 +1,67 @@
-void PROG0_init__(PROG0 *data__, BOOL retain) {
-  __INIT_VAR(data__->VAR_IN,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->VAR_OUT,__BOOL_LITERAL(FALSE),retain)
+void ENERGY_init__(ENERGY *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->J,0,retain)
+  __INIT_VAR(data__->C,0,retain)
+  __INIT_VAR(data__->WH,0,retain)
+  __INIT_VAR(data__->YJ,0,retain)
+  __INIT_VAR(data__->YC,0,retain)
+  __INIT_VAR(data__->YWH,0,retain)
 }
 
 // Code part
-void PROG0_body__(PROG0 *data__) {
+void ENERGY_body__(ENERGY *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
   // Initialise TEMP variables
 
-  __SET_VAR(data__->,VAR_OUT,,__GET_VAR(data__->VAR_IN,));
+  __SET_VAR(data__->,YJ,,((__GET_VAR(data__->J,) + (__GET_VAR(data__->WH,) * 3600.0)) + (__GET_VAR(data__->C,) * 4.1868)));
+  __SET_VAR(data__->,YC,,(__GET_VAR(data__->YJ,) * 0.238845896627496));
+  __SET_VAR(data__->,YWH,,(__GET_VAR(data__->YJ,) * 2.7777777778E-004));
 
   goto __end;
 
 __end:
   return;
-} // PROG0_body__() 
+} // ENERGY_body__() 
+
+
+
+
+
+void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain) {
+  __INIT_VAR(data__->J,0,retain)
+  __INIT_VAR(data__->C,0,retain)
+  __INIT_VAR(data__->WH,0,retain)
+  __INIT_VAR(data__->YJ,0,retain)
+  __INIT_VAR(data__->YC,0,retain)
+  __INIT_VAR(data__->YWH,0,retain)
+  ENERGY_init__(&data__->FUNC_BLOCK,retain);
+}
+
+// Code part
+void PROGRAM0_body__(PROGRAM0 *data__) {
+  // Initialise TEMP variables
+
+  __SET_VAR(data__->,FUNC_BLOCK.J,,__GET_VAR(data__->J,));
+  __SET_VAR(data__->,FUNC_BLOCK.C,,__GET_VAR(data__->C,));
+  __SET_VAR(data__->,FUNC_BLOCK.WH,,__GET_VAR(data__->WH,));
+  ENERGY_body__(&data__->FUNC_BLOCK);
+  __SET_VAR(data__->,YJ,,__GET_VAR(data__->FUNC_BLOCK.YJ,));
+  __SET_VAR(data__->,YC,,__GET_VAR(data__->FUNC_BLOCK.YC,));
+  __SET_VAR(data__->,YWH,,__GET_VAR(data__->FUNC_BLOCK.YWH,));
+
+  goto __end;
+
+__end:
+  return;
+} // PROGRAM0_body__() 
 
 
 
--- webserver/core/POUS.h
+++ webserver/core/POUS.h
@@ -4,18 +4,43 @@
 #include "accessor.h"
 #include "iec_std_lib.h"
 
-// PROGRAM PROG0
+// FUNCTION_BLOCK ENERGY
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(REAL,J)
+  __DECLARE_VAR(REAL,C)
+  __DECLARE_VAR(REAL,WH)
+  __DECLARE_VAR(REAL,YJ)
+  __DECLARE_VAR(REAL,YC)
+  __DECLARE_VAR(REAL,YWH)
+
+  // FB private variables - TEMP, private and located variables
+
+} ENERGY;
+
+void ENERGY_init__(ENERGY *data__, BOOL retain);
+// Code part
+void ENERGY_body__(ENERGY *data__);
+// PROGRAM PROGRAM0
 // Data part
 typedef struct {
   // PROGRAM Interface - IN, OUT, IN_OUT variables
 
   // PROGRAM private variables - TEMP, private and located variables
-  __DECLARE_VAR(BOOL,VAR_IN)
-  __DECLARE_VAR(BOOL,VAR_OUT)
+  __DECLARE_VAR(REAL,J)
+  __DECLARE_VAR(REAL,C)
+  __DECLARE_VAR(REAL,WH)
+  __DECLARE_VAR(REAL,YJ)
+  __DECLARE_VAR(REAL,YC)
+  __DECLARE_VAR(REAL,YWH)
+  ENERGY FUNC_BLOCK;
 
-} PROG0;
+} PROGRAM0;
 
-void PROG0_init__(PROG0 *data__, BOOL retain);
+void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain);
 // Code part
-void PROG0_body__(PROG0 *data__);
+void PROGRAM0_body__(PROGRAM0 *data__);
 #endif //__POUS_H
